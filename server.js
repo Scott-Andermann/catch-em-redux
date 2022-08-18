84c6ebd697389ff5695c59f58cf1ee316c4187be
Scott Andermann <scottandermann@gmail.com>
2022-08-18 11:12:10 -0400

    change port

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const webSocketServer = require("websocket").server;
 const http = require("http");
-const webSocketServerPort = 8080;
+const { env } = require("process");
+const webSocketServerPort = process.env.PORT || 8080;
 
 const server = http.createServer();
-server.listen(webSocketServerPort);
+server.listen(webSocketServerPort, () =>
+  console.log(`listening on ${webSocketServerPort}`)
+);
 
 const wsServer = new webSocketServer({
   httpServer: server,
 });
 
 const clients = {};
-const users = {};
-let userActivity = [];
 
 const generateUniqueId = () => {
   const s4 = () =>
