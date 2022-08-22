8ced36bc25360e6a6a1d473f4290cf56c51a8f39
Scott A <scottandermann@gmail.com>
2022-08-22 14:51:03 -0400

    refactor

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import { motion, AnimatePresence } from 'framer-motion';
 import './Menu.css';
+import ListElement from "./ListElement";
+import { arrow, exitButton } from "../../resources/svgart";
 
 const ArrowDiv = styled.div`
 position: relative;
@@ -25,16 +28,20 @@ const MenuList = styled.ul`
 list-style: none;
 width: 100%;
 height: 100%;
+padding-top: 15vh;
 `
 
-const ListElement = styled.li`
-color: white;
-font-weight: bold;
-font-size: 18px;
+const BreakDiv = styled.div`
+flex-basis: 100%;
+height: 0
 `
+
+const moreElements = ['one', 'two', 'three', 'four']
 
 const Menu = () => {
     const [visible, setVisible] = useState(false);
+    const [showMore, setShowMore] = useState(false);
+    const [showMoreExplore, setShowMoreExplore] = useState(false);
 
     return (
         <div style={{
@@ -54,12 +61,33 @@ const Menu = () => {
                     exit={{ x: '-400px' }}
                     transition={{ type: 'tween' }}>
                     <MenuList>
-                        <ListElement><a href='/'>Home</a></ListElement>
-                        <ListElement><a href='/explore'>Explore</a></ListElement>
-                        <ListElement><a href='/pokedex'>Pokedex</a></ListElement>
+                        <ListElement showMore={showMore} setShowMore={setShowMore} moreElements={moreElements} linkTo='/' heading='Home' />
+                        <ListElement showMore={showMoreExplore} setShowMore={setShowMoreExplore} moreElements={moreElements} linkTo='/explore' heading='Explore' />
+                        {/* <ListElement>
+                            <div style={{ display: 'flex' }}>
+                                <div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer', width: '20px' }} onClick={() => setElemVis(curr => !curr)}>
+                                    {arrow({ height: '18px', width: 'auto', transform: elemVis ? 'rotate(90deg)' : 'rotate(0deg)', transition: 'transform 0.2s' })}
+                                </div>
+                                <Link to='/'>Home</Link>
+                            </div>
+                            <AnimatePresence>
+                                {elemVis && <motion.ul
+                                    key='menu-drop'
+                                    initial={{ lineHeight: 0, opacity: 0 }}
+                                    animate={{ lineHeight: '2rem', opacity: 1 }}
+                                    exit={{ lineHeight: 0, opacity: 0, transition: { lineHeight: { delay: 0.5 } } }}
+                                >
+                                    {elemVis && moreElements.map(element => <motion.li>{element}</motion.li>)}
+                                </motion.ul>}
+                            </AnimatePresence>
+                        </ListElement> */}
+                        {/* <ListElement><Link to='/explore'>Explore</Link></ListElement>
+                        <ListElement><Link to='/pokedex'>Pokedex</Link></ListElement> */}
                     </MenuList>
-                    <svg
-                        style={{height: '40px'}}
+                    <div style={{ height: '40px' }} onClick={() => setVisible(false)}>
+                        {exitButton({ height: '40px', cursor: 'pointer' })}
+                    </div>
+                    {/* <svg
                         xmlns="http://www.w3.org/2000/svg"
                         data-name="Layer 1"
                         viewBox="0 0 122.88 122.88"
@@ -68,29 +96,19 @@ const Menu = () => {
                             d="M6 6a20.53 20.53 0 0129 0l26.5 26.49L87.93 6a20.54 20.54 0 0129 0 20.53 20.53 0 010 29L90.41 61.44l26.49 26.49a20.54 20.54 0 010 29 20.54 20.54 0 01-29 0L61.44 90.41 35 116.9a20.54 20.54 0 01-29 0 20.54 20.54 0 010-29l26.47-26.46L6 34.94A20.53 20.53 0 016 6z"
                             className="cls-1"
                         ></path>
-                    </svg>
+                    </svg> */}
                 </motion.div>}
                 {/* This is the motion component to open the menu */}
-                {!visible && 
-                <motion.div className='arrow' onClick={() => setVisible(true)}
-                    key='menu-selector'
-                    initial={{ width: 0 }}
-                    animate={{ width: '40px' }}
-                    exit={{ width: 0, transition: { delay: 0 } }}
-                    transition={{ delay: 0.3 }}
-                >
-                    <svg
-                        style={{ height: '40px', width: 'auto' }}
-                        xmlns="http://www.w3.org/2000/svg"
-                        x="0"
-                        y="0"
-                        version="1.1"
-                        viewBox="0 0 330 330"
-                        xmlSpace="preserve"
+                {!visible &&
+                    <motion.div className='arrow' onClick={() => setVisible(true)}
+                        key='menu-selector'
+                        initial={{ width: 0 }}
+                        animate={{ width: '40px' }}
+                        exit={{ width: 0, transition: { delay: 0 } }}
+                        transition={{ delay: 0.3 }}
                     >
-                        <path d="M250.606 154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213.001-5.857 5.858-5.857 15.355.001 21.213l139.393 139.39L79.393 304.394c-5.857 5.858-5.857 15.355.001 21.213C82.322 328.536 86.161 330 90 330s7.678-1.464 10.607-4.394l149.999-150.004a14.996 14.996 0 000-21.213z"></path>
-                    </svg>
-                </motion.div>}
+                        {arrow({ height: '40px', width: 'auto' })}
+                    </motion.div>}
             </AnimatePresence>
 
         </div>
---
+++ src/resources/svgart.js
@@ -0,0 +1,36 @@
+
+export const arrow = (style) => {
+
+    return (
+        <svg
+            style={style}
+            xmlns="http://www.w3.org/2000/svg"
+            x="0"
+            y="0"
+            version="1.1"
+            viewBox="0 0 330 330"
+            xmlSpace="preserve"
+        >
+            <path d="M250.606 154.389l-150-149.996c-5.857-5.858-15.355-5.858-21.213.001-5.857 5.858-5.857 15.355.001 21.213l139.393 139.39L79.393 304.394c-5.857 5.858-5.857 15.355.001 21.213C82.322 328.536 86.161 330 90 330s7.678-1.464 10.607-4.394l149.999-150.004a14.996 14.996 0 000-21.213z"></path>
+        </svg>
+    )
+}
+
+export const exitButton = (style) => {
+    return (
+        <svg
+        style={style}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        className="feather feather-x"
+        viewBox="0 0 24 24"
+      >
+        <path d="M18 6L6 18"></path>
+        <path d="M6 6L18 18"></path>
+      </svg>
+    );
+}
